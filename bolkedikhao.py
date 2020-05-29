e17499f69b9b6229115ffc6b84011b0fbf60a7f7
Akashdeep Chitransh <rajsrivastava288@gmail.com>
2020-05-29 18:41:59 +0530

    Another speak string added

--- bolkedikhao.py
+++ bolkedikhao.py
@@ -3,3 +3,5 @@ from win32com.client import Dispatch
 speak = Dispatch("SAPI.SpVoice")
 
 speak.Speak("harry is a good boy")
+
+speak.Speak("Raj somehow you did it best among the best coders of khaga")
